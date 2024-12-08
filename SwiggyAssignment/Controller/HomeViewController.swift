2a12801b9718a929c2f41fa4ae55c9a84bed4f02
bharati1234 <wagh.bharati9213@gmail.com>
2024-12-08 16:02:54 +0530

    button selection functionality is implemented for required sections

--- SwiggyAssignment/Controller/HomeViewController.swift
+++ SwiggyAssignment/Controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  SwiggyAssignment
 //
 //  Created by Bharati on 06/12/24.
@@ -11,16 +11,46 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    // Enum to define sections for better clarity
+    enum HomeSections: Int, CaseIterable {
+        case bannerSlider = 1
+        case quickActions
+        case featuredFood
+        case offers
+        case foodCategories
+        case quickPicks
+        case whatsNew
+        case moreRestaurants
+        case advertisements
+        case filterSortButtons
+        
+        var height: CGFloat {
+            switch self {
+            case .bannerSlider: return 200
+            case .quickActions: return 80
+            case .featuredFood: return 273
+            case .offers: return 200
+            case .foodCategories: return 260
+            case .quickPicks: return 90
+            case .whatsNew: return 520
+            case .moreRestaurants: return 69
+            case .advertisements: return 120
+            case .filterSortButtons : return 80
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
     }
     
-    func setupTableView() {
+    private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: "CustomHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: CustomHeaderView.identifier)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+       
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DefaultCell")
         tableView.register(UINib(nibName: "bannerSliderTableViewCell", bundle: nil), forCellReuseIdentifier: "bannerSliderTableViewCell")
         tableView.register(UINib(nibName: "ButtonsTableViewCell", bundle: nil), forCellReuseIdentifier: "ButtonsTableViewCell")
         tableView.register(UINib(nibName: "FoodTableViewCell", bundle: nil), forCellReuseIdentifier: "FoodTableViewCell")
@@ -29,117 +59,108 @@ class HomeViewController: UIViewController {
         tableView.register(UINib(nibName: "QuickPicsTableViewCell", bundle: nil), forCellReuseIdentifier: "QuickPicsTableViewCell")
         tableView.register(UINib(nibName: "WhatsNewTableViewCell", bundle: nil), forCellReuseIdentifier: "WhatsNewTableViewCell")
         tableView.register(UINib(nibName: "SeeMoreRestaurantTableViewCell", bundle: nil), forCellReuseIdentifier: "SeeMoreRestaurantTableViewCell")
-
+        tableView.register(UINib(nibName: "AdvertiseImgeTableViewCell", bundle: nil), forCellReuseIdentifier: "AdvertiseImgeTableViewCell")
+        tableView.register(UINib(nibName: "FilterSortButtonsTableViewCell", bundle: nil), forCellReuseIdentifier: "FilterSortButtonsTableViewCell")
     }
 }
 
-
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 9
+        return HomeSections.allCases.count + 1 // +1 for optional custom header section
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 0
-        }
-        return 1
+        return section == 0 ? 0 : 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let section = HomeSections(rawValue: indexPath.section) else {
+            return UITableViewCell()
+        }
         
-        if indexPath.section == 1 { // Banner section
-                    guard let cell = tableView.dequeueReusableCell(withIdentifier: "bannerSliderTableViewCell", for: indexPath) as? bannerSliderTableViewCell else {
-                        return UITableViewCell()
-                    }
-                    return cell
-        }else if indexPath.section == 2{
+        switch section {
+        case .bannerSlider:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "bannerSliderTableViewCell", for: indexPath) as? bannerSliderTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        case .quickActions:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "ButtonsTableViewCell", for: indexPath) as? ButtonsTableViewCell else {
                 return UITableViewCell()
             }
             return cell
-        }else if indexPath.section == 3{
+            
+        case .featuredFood:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "FoodTableViewCell", for: indexPath) as? FoodTableViewCell else {
                 return UITableViewCell()
             }
             return cell
-        }else if indexPath.section == 4{
+            
+        case .offers:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "OfferTableViewCell", for: indexPath) as? OfferTableViewCell else {
                 return UITableViewCell()
             }
             return cell
-        }else if indexPath.section == 5{
+            
+        case .foodCategories:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "FoodTypeTableViewCell", for: indexPath) as? FoodTypeTableViewCell else {
                 return UITableViewCell()
             }
             cell.lblSectionTitle.isHidden = false
             return cell
-        }else if indexPath.section == 6{
+            
+        case .quickPicks:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "QuickPicsTableViewCell", for: indexPath) as? QuickPicsTableViewCell else {
                 return UITableViewCell()
             }
-            
             return cell
-        }else if indexPath.section == 7{
-                guard let cell = tableView.dequeueReusableCell(withIdentifier: "WhatsNewTableViewCell", for: indexPath) as? WhatsNewTableViewCell else {
-                    return UITableViewCell()
-                }
-                return cell
-        }else if indexPath.section == 8{
+            
+        case .whatsNew:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "WhatsNewTableViewCell", for: indexPath) as? WhatsNewTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+            
+        case .moreRestaurants:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "SeeMoreRestaurantTableViewCell", for: indexPath) as? SeeMoreRestaurantTableViewCell else {
                 return UITableViewCell()
             }
             return cell
-    }
-      
-           
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Row \(indexPath.row)"
-        return cell
+            
+        case .advertisements:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "AdvertiseImgeTableViewCell", for: indexPath) as? AdvertiseImgeTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+        case .filterSortButtons:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "FilterSortButtonsTableViewCell", for: indexPath) as? FilterSortButtonsTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
+        }
     }
     
-    // Custom header view
-       func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-           if section > 0 {
-               return nil
-           }
-           guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: CustomHeaderView.identifier) as? CustomHeaderView else {
-               return nil
-           }
-           return headerView
-       }
+    // Custom Header View
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard section == 0 else { return nil }
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: CustomHeaderView.identifier) as? CustomHeaderView else {
+            return nil
+        }
+        return headerView
+    }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return section == 0 ? 150 : 0
-       
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            if indexPath.section == 1 {
-                return 200
-            }else if indexPath.section == 2{
-                return 80
-            }else if indexPath.section == 3{
-                return 273
-            }else if indexPath.section == 4{
-                return 200
-            }else if indexPath.section == 5{
-            return 260
-        }else if indexPath.section == 6{
-            return 90
-        }else if indexPath.section == 7{
-            return 520
-        }else if indexPath.section == 8{
-            return 69
-        }
+        guard let section = HomeSections(rawValue: indexPath.section) else {
             return UITableView.automaticDimension
         }
+        return section.height
+    }
 }
-
-//extension HomeViewController: UIScrollViewDelegate {
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        applyStickyHeaderEffect(to: scrollView)
-//    }
-//}
 
 extension HomeViewController {
     func applyStickyHeaderEffect(to scrollView: UIScrollView) {
@@ -151,4 +172,3 @@ extension HomeViewController {
         }
     }
 }
-
---
+++ SwiggyAssignment/CustomTVCells/AdvertiseImgeTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  AdvertiseImgeTableViewCell.swift
+//  SwiggyAssignment
+//
+//  Created by Sumit Aquil on 08/12/24.
+//
+
+import UIKit
+
+class AdvertiseImgeTableViewCell: UITableViewCell {
+
+   
+    @IBOutlet weak var advertiseImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+  
+}
